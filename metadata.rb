58790c6f30b0cbf52efe5a70b3b1e771540fc966
Masaki Matsushita <glass.saga@gmail.com>
2014-10-15 17:30:38 +0900

    version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'glass.saga@gmail.com'
 license          'All rights reserved'
 description      'Set up my development environments'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.8'
+version          '0.1.9'
 depends          'homebrew'
 depends          'sudo'
 depends          'yum'
